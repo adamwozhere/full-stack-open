905e57e58f48c73079e26914127db4c4acdef84f
Adam Wozniak <adamwozhere@gmail.com>
2023-09-16 19:51:17 +0100

    part3 pre-deploy

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body;
+
+  if (!person.name || !person.number) {
+    return response.status(400).json({ error: 'Name or Number is missing' });
+  }
+
+  const nameAlreadyExists = persons.find(
+    (listed) => listed.name == person.name
+  );
+  if (nameAlreadyExists) {
+    return response.status(400).json({ error: 'Name must be unique' });
+  }
+
+  person.id = Math.floor(Math.random() * 1000);
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${
+      persons.length
+    } people</p><p>${new Date().toString()}</p>`
+  );
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+//approx 3hr
+
