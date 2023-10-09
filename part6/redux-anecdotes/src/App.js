f3085ae0b3249d174d1fd01ffd0c8c86e61723c6
Adam Wozniak <adamwozhere@gmail.com>
2023-10-09 18:06:31 +0100

    exercise 6.14

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
 import Notification from './components/Notification';
+import anecdoteService from './services/anecdotes';
+import { setAnecdotes } from './reducers/anecdoteReducer';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -18,6 +29,6 @@ const App = () => {
 
 export default App;
 
-// approx 4hr 30min - finished exercise 6.10 nad 6.11 (they seem exactly the same tasks?)
+// approx 5hr - finished exercise 6.14
 // installed redux chrome extension but not sure how to use it properly
 
