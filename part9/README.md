f6da2bccfd73d178a8953ef50703b0610f4773bb
Adam Wozniak <adamwozhere@gmail.com>
2023-10-26 17:03:18 +0100

    exercise 9.1

---
+++ part9/README.md
@@ -0,0 +1,5 @@
+# Part 9
+
+## exercises 9.1 -
+
+exercise 9.1 = 1hr 30min
---
+++ part9/calculators/bmiCalculator.ts
@@ -0,0 +1,46 @@
+interface HeightAndWeight {
+  height: number;
+  weight: number;
+}
+
+const parseArgs = (args: string[]): HeightAndWeight => {
+  if (args.length < 4) throw new Error('Not enought arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
+const calculateBmi = (heightCM: number, weightKG: number) => {
+  const heightM = heightCM / 100;
+  let bmi = weightKG / (heightM * heightM);
+  let result = '';
+
+  if (bmi < 18.5) {
+    result = 'Underweight';
+  } else if (bmi < 25) {
+    result = 'Normal (healthy weight)';
+  } else if (bmi < 30) {
+    result = 'Overweight';
+  } else {
+    result = 'Obese';
+  }
+
+  console.log(`bmi: ${bmi.toFixed(2)} - ${result}`);
+};
+
+try {
+  const { height, weight } = parseArgs(process.argv);
+  calculateBmi(height, weight);
+} catch (error) {
+  if (error instanceof Error) {
+    console.log('Error:', error.message);
+  }
+}
+
