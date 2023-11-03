e6a511709e08726a3a31b12f9364965e4129029f
Adam Wozniak <adamwozhere@gmail.com>
2023-11-03 15:49:54 +0000

    exercise 9.19

--- part9/flight-diary/frontend/src/components/NewDiaryForm.tsx
+++ part9/flight-diary/frontend/src/components/NewDiaryForm.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 const NewDiaryForm = ({ handleNewDiary }: Props) => {
   const [date, setDate] = useState('');
-  const [visibility, setVisibility] = useState('');
-  const [weather, setWeather] = useState('');
+  const [visibility, setVisibility] = useState('great');
+  const [weather, setWeather] = useState('sunny');
   const [comment, setComment] = useState('');
 
   const handleSubmit = (e: React.SyntheticEvent) => {
@@ -31,33 +31,100 @@ const NewDiaryForm = ({ handleNewDiary }: Props) => {
       <div>
         <label htmlFor="date">Date</label>
         <input
-          type="text"
+          type="date"
           name="date"
           id="date"
           value={date}
           onChange={({ target }) => setDate(target.value)}
         />
       </div>
-      <div>
-        <label htmlFor="visibility">Visibility</label>
+      <fieldset>
+        <legend>Visibility</legend>
+        <label htmlFor="great">Great</label>
         <input
-          type="text"
+          type="radio"
           name="visibility"
-          id="visibility"
-          value={visibility}
+          id="great"
+          value="great"
+          checked={visibility === 'great'}
           onChange={({ target }) => setVisibility(target.value)}
         />
-      </div>
-      <div>
-        <label htmlFor="weather">Weather</label>
+        <label htmlFor="good">Good</label>
         <input
-          type="text"
+          type="radio"
+          name="visibility"
+          id="good"
+          value="good"
+          checked={visibility === 'good'}
+          onChange={({ target }) => setVisibility(target.value)}
+        />
+        <label htmlFor="ok">Ok</label>
+        <input
+          type="radio"
+          name="visibility"
+          id="ok"
+          value="ok"
+          checked={visibility === 'ok'}
+          onChange={({ target }) => setVisibility(target.value)}
+        />
+        <label htmlFor="poor">Poor</label>
+        <input
+          type="radio"
+          name="visibility"
+          id="poor"
+          value="poor"
+          checked={visibility === 'poor'}
+          onChange={({ target }) => setVisibility(target.value)}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>Weather</legend>
+        <label htmlFor="sunny">Sunny</label>
+        <input
+          type="radio"
           name="weather"
-          id="weather"
-          value={weather}
+          id="sunny"
+          value="sunny"
+          checked={weather === 'sunny'}
           onChange={({ target }) => setWeather(target.value)}
         />
-      </div>
+        <label htmlFor="rainy">Rainy</label>
+        <input
+          type="radio"
+          name="weather"
+          id="rainy"
+          value="rainy"
+          checked={weather === 'rainy'}
+          onChange={({ target }) => setWeather(target.value)}
+        />
+        <label htmlFor="cloudy">Cloudy</label>
+        <input
+          type="radio"
+          name="weather"
+          id="cloudy"
+          value="cloudy"
+          checked={weather === 'cloudy'}
+          onChange={({ target }) => setWeather(target.value)}
+        />
+        <label htmlFor="stormy">Stormy</label>
+        <input
+          type="radio"
+          name="weather"
+          id="stormy"
+          value="stormy"
+          checked={weather === 'stormy'}
+          onChange={({ target }) => setWeather(target.value)}
+        />
+        <label htmlFor="windy">Windy</label>
+        <input
+          type="radio"
+          name="weather"
+          id="windy"
+          value="windy"
+          checked={weather === 'windy'}
+          onChange={({ target }) => setWeather(target.value)}
+        />
+      </fieldset>
       <div>
         <label htmlFor="comment">Comment</label>
         <input
